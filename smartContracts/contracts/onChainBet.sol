73dd92fbe2dfe045d2a71f3b14636c4a073549b2
Allan Heremi <allanheremidev@gmail.com>
2023-09-13 09:35:31 +0200

    Feat: Solidity random function for int 0-99

--- smartContracts/contracts/onChainBet.sol
+++ smartContracts/contracts/onChainBet.sol
@@ -1,9 +1,20 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.2 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 contract onChainBet {
-    
-    
-}
+    uint randNone = 0;
 
+    uint public result = 0;
+
+    function randMod() public {
+        randNone++;
+        result =
+            uint(
+                keccak256(
+                    abi.encodePacked(block.timestamp, msg.sender, randNone)
+                )
+            ) %
+            100;
+    }
+}
