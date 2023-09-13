b5abc24547c059aa49dcb19b6426bc99f878c232
Allan Heremi <allanheremidev@gmail.com>
2023-09-13 10:43:58 +0200

    Co-authored-by: rofunn <rofunn@users.noreply.github.com>

--- smartContracts/contracts/onChainBet.sol
+++ smartContracts/contracts/onChainBet.sol
@@ -3,8 +3,30 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 contract onChainBet {
-    uint randNone = 0;
+    address public admin;
+    mapping(address => uint) balances;
 
+    // Setting the deployer address as the admin address
+    constructor() {
+        admin = msg.sender;
+    }
+
+    // Deposit function [To be implemented]
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    // Withdraw function [To be implemented]
+    function withdraw(uint amount) public {
+        require(msg.sender == admin, "Not authorized to withdraw");
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+
+    // Randomize function
+    uint randNone = 0;
     uint public result = 0;
 
     function randMod() public {
@@ -17,4 +39,6 @@ contract onChainBet {
             ) %
             100;
     }
+
+    // Betting logic [To be implemented]
 }
