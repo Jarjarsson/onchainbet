1a7b9daea24b1b7a8d54e935d34f25701c2f5041
Allan Heremi <allanheremidev@gmail.com>
2023-09-15 11:24:43 +0200

    Feat: UI slider for multiplier input

--- app/bet/page.tsx
+++ app/bet/page.tsx
@@ -7,8 +7,8 @@ const Bet = () => {
   const [address, setAddress] = useState("");
   const [wallet, setWallet] = useState("");
   const [contractBalance, setContractBalance]=useState("")
-  const [multiplier,setMultiplier]=useState(0)
-  const [sliderValue, setSliderValue] = useState(5)
+  const [multiplier,setMultiplier]=useState(2)
+  
 
   const handleBet = (multiplier: number) => {
      placeBet(contractAddress, wallet,  multiplier, 1000000000000000);
@@ -25,18 +25,14 @@ const Bet = () => {
   return (
     <>
       <p>Multiplier</p>
-      <select name="multiplier" onChange={(e)=>{setMultiplier(Number(e.target.value))}}>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="10">10</option>
-      </select>
+
       <button onClick={() => handleBet(multiplier)}>BET</button><p>{wallet}</p>;
       <button onClick={() => handleSelectWallet()}>CONNECT</button>
       <button onClick={() => console.log(ethToWei(0.2))}>test</button>
-      <p>{sliderValue}</p>
-      <input type="range" value={sliderValue} min={2} max={10} onChange={e => setSliderValue(Number(e.target.value))} />
+
+    
+      <p>{multiplier}</p>
+      <input type="range" value={multiplier} min={2} max={10} onChange={e => setMultiplier(Number(e.target.value))} />
     </>
   );
 };
