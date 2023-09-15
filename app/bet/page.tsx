00ac7cd4ee8948a638c4cde0bc88868837a64c28
Allan Heremi <allanheremidev@gmail.com>
2023-09-15 10:59:54 +0200

    Feat: SC interaction in UI
    Co-authored-by: Rasmus Eklund <rasmus-eklund@users.noreply.github.com>
    Co-authored-by: rofunn <rofunn@users.noreply.github.com>

--- app/bet/page.tsx
+++ app/bet/page.tsx
@@ -1,15 +1,19 @@
 'use client'
 import React, { useState } from "react";
-import { connectWallet, placeBet } from "./web3/web3Client";
+import { getContractBalance, connectWallet, placeBet, ethToWei } from "./web3/web3Client";
 import contractAddress from "./constants/address";
 
 const Bet = () => {
   const [address, setAddress] = useState("");
   const [wallet, setWallet] = useState("");
+  const [contractBalance, setContractBalance]=useState("")
+  const [multiplier,setMultiplier]=useState(0)
 
   const handleBet = (multiplier: number) => {
-    // placeBet(contractAddress, address,  100000, 2);
+     placeBet(contractAddress, wallet,  multiplier, 1000000000000000);
   };
+  
+
 
   const handleSelectWallet = async () => {
     await connectWallet().then((res) => {
@@ -19,8 +23,17 @@ const Bet = () => {
 
   return (
     <>
-      <button onClick={() => handleBet(3)}>BUTTON</button>;helllo<p>{wallet}</p>;
+      <p>Multiplier</p>
+      <select name="multiplier" onChange={(e)=>{setMultiplier(Number(e.target.value))}}>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="10">10</option>
+      </select>
+      <button onClick={() => handleBet(multiplier)}>BET</button><p>{wallet}</p>;
       <button onClick={() => handleSelectWallet()}>CONNECT</button>
+      <button onClick={() => console.log(ethToWei(0.2))}>test</button>
     </>
   );
 };
--- app/bet/web3/web3Client.ts
+++ app/bet/web3/web3Client.ts
@@ -1,11 +1,12 @@
-import Web3 from "web3";
-import abi from "../constants/abi";
-import contractAddress from "../constants/address";
+import Web3, { Web3BaseProvider, providers } from 'web3';
+import abi from '../constants/abi';
+import contractAddress from '../constants/address';
 
 const web3 = new Web3(
-  "wss://eth-sepolia.g.alchemy.com/v2/ET7Rh8pt9Djc9dEaYvVcxhWd5EtIn3PK"
+  'wss://eth-sepolia.g.alchemy.com/v2/ET7Rh8pt9Djc9dEaYvVcxhWd5EtIn3PK'
 );
 
+const ethToWei = (eth: number) => Web3.utils.toWei(eth.toString(), 'ether');
 const onChainBet = new web3.eth.Contract(abi, contractAddress);
 
 const placeBet = async (
@@ -15,33 +16,37 @@ const placeBet = async (
   amount: number
 ) => {
   const transactionParameters = {
-    to: contractAddress,
     from: address,
-    data: onChainBet.methods.placeBet().encodeABI(),
-    value: amount,
-    multiplier: multiplier
+    to: contractAddress,
+    data: onChainBet.methods
+      .placeBet(Number(multiplier).toString(16))
+      .encodeABI(),
+    value: amount.toString(16),
   };
-
   try {
     const txHash = await (window as any).ethereum.request({
-      method: "eth_sendTransaction",
+      method: 'eth_sendTransaction',
       params: [transactionParameters],
     });
     return {
-      status: "Success! Transaction hash: " + txHash,
+      status: 'Success! Transaction hash: ' + txHash,
     };
   } catch (error) {
     return {
-      status: "Something went wrong",
+      status: 'Something went wrong',
     };
   }
+};
+
+const getContractBalance = async () => {
+  return web3.eth.getBalance(contractAddress);
 };
 
 const connectWallet = async () => {
   if ((window as any).ethereum) {
     try {
       const addressArray = await (window as any).ethereum.request({
-        method: "eth_requestAccounts",
+        method: 'eth_requestAccounts',
       });
       const obj = {
         connected: true,
@@ -50,16 +55,16 @@ const connectWallet = async () => {
       return obj;
     } catch (err) {
       return {
-        address: "",
+        address: '',
         connected: false,
       };
     }
   } else {
     return {
-      address: "",
+      address: '',
       connected: false,
     };
   }
 };
 
-export { placeBet, connectWallet };
+export { placeBet, connectWallet, getContractBalance, ethToWei };
