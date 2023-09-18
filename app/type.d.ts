9ea224d00a5a69b9515cb45e1c35c94a55429e92
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-09-18 10:18:27 +0200

    chore: add type for result
    Co-authored-by: rofunn <rofunn@users.noreply.github.com>

--- app/type.d.ts
+++ app/type.d.ts
@@ -2,3 +2,10 @@ export type Result = {
   status: string;
   amount: number;
 };
+
+export type ReturnValues = {
+  amount: bigint,
+  outcome: bigint,
+  playerAddress: string,
+  status: 'Loss' | 'Win'
+}
