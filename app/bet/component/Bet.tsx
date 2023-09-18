b26846a10ee50051b8847c7bb109fe374e77538f
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-09-18 13:23:15 +0200

    fix: bet color

--- app/bet/component/Bet.tsx
+++ app/bet/component/Bet.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import contractAddress from "../constants/address";
-import { useTContext } from "../../context/Context";
-import { gameResult, getMaxBet, placeBet } from "../web3/web3Client";
-import { ReturnValues } from "@/app/type";
+import contractAddress from '../constants/address';
+import { useTContext } from '../../context/Context';
+import { gameResult, getMaxBet, placeBet } from '../web3/web3Client';
+import { ReturnValues } from '@/app/type';
 
 const Bet = () => {
   const [multiplier, setMultiplier] = useState(2);
@@ -11,7 +11,7 @@ const Bet = () => {
   const [amount, setAmount] = useState(0.000000001);
   const [maxAmount, setMaxAmount] = useState(0);
   const [loadingBet, setLoadingBet] = useState(false);
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState('');
   useEffect(() => {
     (async () => {
       setMaxAmount(await getMaxBet());
@@ -36,13 +36,13 @@ const Bet = () => {
       console.log(status);
       setMaxAmount(await getMaxBet());
       setLoadingBet(true);
-      setResult("");
+      setResult('');
     }
   };
 
   return (
     <form
-      onSubmit={(e) => {
+      onSubmit={e => {
         e.preventDefault();
         handleBet(multiplier);
       }}
@@ -55,14 +55,15 @@ const Bet = () => {
         value={multiplier}
         min={2}
         max={10}
-        onChange={(e) => setMultiplier(Number(e.target.value))}
+        onChange={e => setMultiplier(Number(e.target.value))}
       />
       <label htmlFor="betAmount">Bet Amount (max bet: {maxAmount})</label>
       <input
+        className="text-black"
         id="betAmount"
         required
         value={amount}
-        onChange={(e) => setAmount(Number(e.target.value))}
+        onChange={e => setAmount(Number(e.target.value))}
         type="number"
         step={0.000000001}
         min={0.000000001}
@@ -73,7 +74,7 @@ const Bet = () => {
       />
       <button> Place bet </button>
       {loadingBet && <p>Getting result</p>}
-      {result === "" ? <p></p> : <p>{result}</p>}
+      {result === '' ? <p></p> : <p>{result}</p>}
     </form>
   );
 };
