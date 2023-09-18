950c8d1c33cadcd04a69bb2adde477e7fbc3d16d
rofun <joufang.w@gmail.com>
2023-09-18 22:02:21 +0800

    Feat: landing page CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex flex-col gap-2 ">
-        <Link href={"/bet"}>Bet</Link>
-        <Link href={"/demo"}>Demo version</Link>
-      </div>
-    </main>
+    <>
+      <header>
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={80}
+          height={80}
+          className="rounded-md"
+        />
+      </header>
+      <main>
+        <div className="flex flex-col gap-10 justify-center items-center pt-16 ">
+          <Link
+            href={"/bet"}
+            className="text-cc3 text-4xl font-bold bg-cc3/50 px-3 py-2 rounded-lg"
+          >
+            BET
+          </Link>
+          <Link
+            href={"/demo"}
+            className="text-cc3 text-4xl font-bold bg-cc3/50 px-3 py-2 rounded-lg"
+          >
+            DEMO VERSION
+          </Link>
+        </div>
+      </main>
+    </>
   );
 }
