7e13b567bc07ae72fd278dbadacbb0238447e9e5
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-09-19 17:14:23 +0200

    fix: move utils and update

--- app/bet/utils/utils.ts
+++
@@ -1,5 +0,0 @@
-export const cropWallet = (wallet: string) =>
-  `${wallet.substring(0, 4)}...${wallet.substring(
-    wallet.length - 4,
-    wallet.length
-  )}`;
---
+++ app/utils/utils.ts
@@ -0,0 +1,26 @@
+import { HistoryItem } from '../type';
+
+export const cropWallet = (wallet: string) =>
+  `${wallet.substring(0, 4)}...${wallet.substring(
+    wallet.length - 4,
+    wallet.length
+  )}`;
+
+export const storeHistory = () => {
+  const key = 'bettingHistory';
+  const update = (item: HistoryItem) => {
+    localStorage.setItem(key, JSON.stringify([...read(), item]));
+  };
+  const read = () => {
+    let historyItems: HistoryItem[] = [];
+    const data = localStorage.getItem(key);
+    if (data !== null) {
+      historyItems = JSON.parse(data) as HistoryItem[];
+    }
+    return historyItems;
+  };
+  const clear = () => {
+    localStorage.setItem(key, JSON.stringify([]));
+  };
+  return {update, read, clear}
+};
