edd7ecb916bad376d5ebd0e84f5e6b1ea5c966ae
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-09-20 16:52:01 +0200

    fix: refactor

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -2,30 +2,40 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Header = () => {
+type HeaderProps = {
+  links: { name: string; url: string }[];
+  children?: React.ReactNode;
+};
+
+const Header = ({ links, children }: HeaderProps) => {
   return (
-    <header className="flex justify-between px-8 py-2 items-center ">
-      <Image
-        src="/logo.png"
-        alt="logo"
-        width={80}
-        height={80}
-        className="rounded-md"
-      />
-      <nav className="flex gap-3">
-        <Link
-          href={'/'}
-          className="text-cc3 text-xl font-semibold bg-cc3/50 px-3 py-2 rounded-lg"
-        >
-          HOME
+    <header className="flex justify-between py-1 items-center border-b border-cc3/30">
+      <div className="flex gap-5 items-center">
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={80}
+          height={80}
+          className="rounded-md"
+        />
+        <Link href={'/'} className="text-cc3 text-2xl">
+          OnChainBet
         </Link>
-        <Link
-          href={'/demo'}
-          className="text-cc3 text-xl font-semibold bg-cc3/50 px-3 py-2 rounded-lg"
-        >
-          DEMO
-        </Link>
-      </nav>
+      </div>
+      <div className='flex items-center gap-10 px-4'>
+        <nav className="flex gap-3">
+          {links.map(({ name, url }) => (
+            <Link
+              key={name}
+              href={url}
+              className="text-cc3 text-xl font-semibold underline"
+            >
+              {name}
+            </Link>
+          ))}
+        </nav>
+        {children}
+      </div>
     </header>
   );
 };
