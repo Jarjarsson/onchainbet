12792ba1c76af297a241d6cfac158a530cc7cbe9
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-09-20 16:50:35 +0200

    fix: gradient background and limit width on lg
    Co-authored-by: rofunn <rofunn@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { TContextProvider } from "./context/Context";
-import Footer from "./component/Footer";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { TContextProvider } from './context/Context';
+import Footer from './component/Footer';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "OnChainBet",
-  description: "Make the game fun and fair",
+  title: 'OnChainBet',
+  description: 'Make the game fun and fair',
 };
 
 export default function RootLayout({
@@ -17,11 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="bg-gradient-to-br from-cc1 from-30% to-cc2 flex flex-col justify-between w-screen h-screen ">
+    <html lang="en" className="flex justify-center">
+      <body className="bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-cc1 to-cc2 w-screen h-screen flex flex-col justify-between lg:max-w-5xl">
         <TContextProvider>{children}</TContextProvider>
         <Footer />
       </body>
     </html>
   );
 }
+
+//bg-gradient-to-br from-cc1 from-30% via-cc2/50 via-50% to-cc1 to-70%
+//bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-gray-200 via-gray-400 to-gray-600
+//bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-gray-200 via-gray-400 to-gray-600
